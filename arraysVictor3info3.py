328b24ad78803e42d4d76521207e10b6ce79772f
Victor Hugo Casett Agostini <101749815+VictorInfoDev@users.noreply.github.com>
2022-04-14 09:30:46 -0300

    Update arraysVictor3info3.py

--- arraysVictor3info3.py
+++ arraysVictor3info3.py
@@ -1,10 +1,9 @@
 #1
 num = 0
-numList = 0
 lista = []
 for i in range(99):
-  num = num + 1
   lista.append(num)
+  num = num + 1
 
 print(lista)
 
@@ -80,4 +79,50 @@ for i in range(len(numLista)):
 
 print(somaLista)
 
+#7
+listaNum = []
+num = -100
+for i in range(201):
+  listaNum.append(num)
+  num = num + 1
 
+n = len(listaNum)
+
+for t in range(len(listaNum)):
+  n = n - 1
+  print(listaNum[n])
+
+#8
+listaNum1 = []
+listaNum2 = []
+listaNum3 = []
+num = 1
+t = 0
+for i in range(5):
+  listaNum1.append(num)
+  listaNum2.append(num)
+  listaNum3.append(listaNum1[i] + listaNum2[i])
+  t = t + 1
+  num = num + 1
+
+print("Vetor 1:",listaNum1)
+print("Vetor 2:",listaNum2)
+print("Vetor 3:",listaNum3)
+
+#15
+vet = []
+for i in range(21):
+  vet.append(i)
+
+print(vet)
+
+#18
+matriz = [["Linha 1",1,2,3,4,5,6,7,8,9,10],["Linha 2",1,2,3,4,5,6,7,8,9,10],["Linha 3",1,2,3,4,5,6,7,8,9,10],["Linha 4",1,2,3,4,5,6,7,8,9,10],["Linha 5",1,2,3,4,5,6,7,8,9,10]]
+for i in range(5):
+  print(matriz[i])
+
+
+
+
+  
+  
